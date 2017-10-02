7e77d236712f253574e8e1c761c5c91289aa7b2e
Blake Parmeter <BlakeParmeter@gmail.com>
2017-10-02 19:41:50 -0400

    added layer for cars

--- src/main/java/GUI/World.java
+++ src/main/java/GUI/World.java
@@ -1,10 +1,13 @@
 package GUI;
 
 
-import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import model.Car;
+import model.CardinalDirection;
 import model.Grid;
 import model.Intersection;
 
@@ -22,24 +25,34 @@ public class World {
     
     private static final int STREET_WIDTH = 10; //px
     
-    private final Canvas canvas;
+    private final StackPane root = new StackPane();
+    private final StackPane carLayer = new StackPane();
+    private final Canvas gridPane;
+    
     
     public World(Grid grid){
-        canvas = new Canvas(grid.getEWBlockSize() * Grid.INTERSECTION_DISATANCE, grid.getNSBlockSize() * Grid.INTERSECTION_DISATANCE);
+        gridPane = new Canvas(grid.getEWBlockSize() * Grid.INTERSECTION_DISATANCE, grid.getNSBlockSize() * Grid.INTERSECTION_DISATANCE);
         drawGrid(grid);
+        
+        //adds all the layers to the root
+        root.getChildren().setAll(gridPane, carLayer);
     }
     
-    public Canvas getCanvas(){
-        return canvas;
+    public Node getRoot(){
+        return root;
+    }
+    
+    private void addCar(Car car){
+        
     }
     
     private void drawGrid(Grid grid){
         
         //Draws the black background
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = gridPane.getGraphicsContext2D();
         int width = grid.getEWBlockSize() * Grid.INTERSECTION_DISATANCE;
         int height = grid.getEWBlockSize() * Grid.INTERSECTION_DISATANCE;
-        gc.setFill(Color.BLACK);
+        gc.setFill(Color.WHITE);
         gc.fillRect(0, 0, width, height);
         
         //Draws all the intersections
@@ -53,7 +66,7 @@ public class World {
     private void drawIntersection(Intersection intersection){
         
         //sets up variables
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = gridPane.getGraphicsContext2D();
         int gridSize = Grid.INTERSECTION_DISATANCE;
         int cellX = intersection.getEWBlock() * gridSize;
         int cellY = intersection.getNSBlock()* gridSize;
@@ -63,7 +76,8 @@ public class World {
         int NSy = cellY;
         int NSw = STREET_WIDTH;
         int NSh = gridSize;
-        gc.setFill(Color.GRAY);
+        //gc.setFill(intersection.getNSDirection() == CardinalDirection.NORTH ? Color.LIGHTGRAY : Color.DARKGRAY); //TODO: integrate the direction branch
+        gc.setFill(Color.LIGHTGRAY);
         gc.fillRect(NSx, NSy, NSw, NSh);
         
         //draws the EW street
@@ -71,7 +85,20 @@ public class World {
         int EWy = cellY + (gridSize / 2) - (STREET_WIDTH / 2);
         int EWw = gridSize;
         int EWh = STREET_WIDTH;
-        gc.setFill(Color.GRAY);
+        gc.setFill(Color.LIGHTGRAY);
+        //gc.setFill(intersection.getEWDirection()== CardinalDirection.EAST ? Color.LIGHTGRAY : Color.DARKGRAY); //TODO: integrate the direction branch
         gc.fillRect(EWx, EWy, EWw, EWh);
+        
+        //draws the middle of the intersection
+        int Mx = cellX + (gridSize / 2) - (STREET_WIDTH / 2);
+        int My = cellY + (gridSize / 2) - (STREET_WIDTH / 2);
+        int Mw = STREET_WIDTH;
+        int Mh = STREET_WIDTH;
+        gc.setFill(Color.BLACK);
+        gc.fillRect(Mx, My, Mw, Mh);
+        
+        //draws the coordinate
+        gc.setStroke(Color.BLUE);
+        gc.strokeText("("+intersection.getEWBlock() + "," + intersection.getNSBlock() + ")", cellX+10, cellY+30);
     }
 }
